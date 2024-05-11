607fe1b6b0322e340fe265f5881565f5eb29227e
danielgen01 <gendik.waf@gmail.com>
2024-05-11 11:38:30 +0200

    feat: add some responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ import { headlineItemsMockCard } from "@/headlineItems.mock"
 export default function HomePageAKADashboardPage() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-10 mt-10 px-16">
+      <div className="grid grid-cols-1 gap-10 mt-10 px-4 xl:px-16 md:grid-cols-2 xl:grid-cols-3 ">
         <Card title="Balance" amount={94242} performance={20} />
         <Card title="Incomes" amount={9500} performance={20} />
         <Card title="Expenses" amount={2500} performance={10} />
       </div>
-      <div className="flex w-full justify-between StyledHeadlineAndFilterWrapper px-16 mt-10 ">
+      <div className="flex w-full justify-between StyledHeadlineAndFilterWrapper px-4 xl:px-16 mt-10 ">
         <h1 className="text-4xl font-semibold">Income Statement</h1>
         <FilterMenu />
       </div>
-      <section className="big-cards-wrapper-section grid grid-cols-2 px-14 gap-10 mt-10 ">
+      <section className="big-cards-wrapper-section grid grid-cols-1 px-4 xl:px-16 gap-10 mt-10 md:grid-cols-2  ">
         <BigCard
           headlineItems={headlineItemsMockCard}
           color="red"
