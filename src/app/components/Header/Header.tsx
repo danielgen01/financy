85c467e1626124e49932572396d4def506fedc3a
danielgen01 <gendik.waf@gmail.com>
2024-05-19 16:19:06 +0200

    feat: replace Styled-JSX with modules

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,27 +1,24 @@
 "use client"
 import React from "react"
-import {
-  StyledAccountActionsWrapper,
-  StyledHeaderWrapper,
-  StyledMenuList,
-} from "./Header.styles"
+
 import Image from "next/image"
 import MenuItem from "./MenuItem"
 import { faBell, faGear } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from "./Header.styles.module.css" // Stelle sicher, dass der Pfad korrekt ist
 
 const Header = () => {
   return (
-    <StyledHeaderWrapper>
-      <Image src={"/Logo.png"} alt={""} width={150} height={50} />
-      <StyledMenuList>
+    <div className={styles.StyledHeaderWrapper}>
+      <Image src={"/Logo.png"} alt={"Logo_Financy"} width={150} height={50} />
+      <ul className={styles.StyledMenuList}>
         <MenuItem label="Overview" isActive={true}></MenuItem>
         <MenuItem label="Transactions"></MenuItem>
         <MenuItem label="Analytics"></MenuItem>
         <MenuItem label="Accounts"></MenuItem>
         <MenuItem label="Wallet"></MenuItem>
-      </StyledMenuList>
-      <StyledAccountActionsWrapper>
+      </ul>
+      <div className={styles.StyledAccountActionsWrapper}>
         <FontAwesomeIcon icon={faGear} fontSize={"1.5rem"} cursor={"pointer"} />
         <FontAwesomeIcon icon={faBell} fontSize={"1.5rem"} cursor={"pointer"} />
         <div className="flex mx-auto overflow-hidden">
@@ -30,11 +27,11 @@ const Header = () => {
             height={40}
             width={40}
             alt="Dummy Image"
-            className="rounded-full aspect-square object-cover"
+            className={styles.StyledAccountProfileImage}
           />
         </div>
-      </StyledAccountActionsWrapper>
-    </StyledHeaderWrapper>
+      </div>
+    </div>
   )
 }
 
