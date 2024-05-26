880a67836401a989d9c36fa301eb61bbf797005e
danielgen01 <gendik.waf@gmail.com>
2024-05-26 15:55:29 +0200

    chore: comment out throw error function

--- src/fetcher/Dashboard/fetchDashboardPage.ts
+++ src/fetcher/Dashboard/fetchDashboardPage.ts
@@ -11,24 +11,30 @@ export async function fetchDashboardPageProps(fetcher: {
 }> {
   const incomeData = await fetcher.getIncomeData()
 
-  if (!incomeData) {
-    throw new Error("No income data found")
-  }
+  // TODO: the data we fetch here is dynamic and doesnt have to be here in the first place,
+  //  so we probably dont need to throw an error
+  // if (!incomeData) {
+  //   throw new Error("No income data found")
+  // }
 
   const expenseData = await fetcher.getExpenseData()
   if (!expenseData) {
     throw new Error("No expense data found")
   }
 
+  // TODO: the data we fetch here is dynamic and doesnt have to be here in the first place,
+  //  so we probably dont need to throw an error
   const assetData = await fetcher.getAssetData()
-  if (!assetData) {
-    throw new Error("No asset data found")
-  }
+  // if (!assetData) {
+  //   throw new Error("No asset data found")
+  // }
 
+  // TODO: the data we fetch here is dynamic and doesnt have to be here in the first place,
+  //  so we probably dont need to throw an error
   const liabilitiesData = await fetcher.getLiabilitiesData()
-  if (!liabilitiesData) {
-    throw new Error("No liabilities data found")
-  }
+  // if (!liabilitiesData) {
+  //   throw new Error("No liabilities data found")
+  // }
 
   return {
     incomeData,
