1a4b78d5dd100485c44f0858d96b2893171f35c1
danielgen01 <gendik.waf@gmail.com>
2024-08-16 15:41:04 +0200

    feat:setup base eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function HomePageAKADashboardPage() {
     });
 
     return () => unsubscribeAuth();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const setupRealtimeListeners = (uid: string) => {
