4ec3d467de0bf76b9115b6f2a4b99b7c8b74445c
danielgen01 <gendik.waf@gmail.com>
2024-08-19 22:19:23 +0200

    feat: add story for dashboardPage

--- src/app/components/pageTemplates/homepage/utilities.ts
+++ src/app/components/pageTemplates/homepage/utilities.ts
@@ -2,39 +2,39 @@ export const calculateTotal = ({
   incomeData,
   expenseData,
 }: {
-  incomeData: any
-  expenseData: any
+  incomeData: any;
+  expenseData: any;
 }) => {
-  let total = 0
+  let total = 0;
   for (const key in incomeData) {
     if (Object.prototype.hasOwnProperty.call(incomeData, key)) {
-      total += incomeData[key].cashflowAmount
+      total += incomeData[key].cashflowAmount;
     }
   }
   for (const key in expenseData) {
     if (Object.prototype.hasOwnProperty.call(expenseData, key)) {
-      total -= expenseData[key].cashflowAmount
+      total -= expenseData[key].cashflowAmount;
     }
   }
-  return total
-}
+  return total;
+};
 
 export const calculateTotalIncome = (incomeData: any) => {
-  let total = 0
+  let total = 0;
   for (const key in incomeData) {
     if (Object.prototype.hasOwnProperty.call(incomeData, key)) {
-      total += incomeData[key].cashflowAmount
+      total += incomeData[key].cashflowAmount;
     }
   }
-  return total
-}
+  return total;
+};
 
 export const calculateTotalExpense = (expenseData: any) => {
-  let total = 0
+  let total = 0;
   for (const key in expenseData) {
     if (Object.prototype.hasOwnProperty.call(expenseData, key)) {
-      total += expenseData[key].cashflowAmount
+      total += expenseData[key].cashflowAmount;
     }
   }
-  return total
-}
+  return total;
+};
