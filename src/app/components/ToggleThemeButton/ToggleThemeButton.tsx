e06bec22ba6e60df169011cc29086523a8b485e9
danielgen01 <gendik.waf@gmail.com>
2024-09-08 13:58:00 +0200

    feat: use prefered color scheme

--- src/app/components/ToggleThemeButton/ToggleThemeButton.tsx
+++ src/app/components/ToggleThemeButton/ToggleThemeButton.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 
 export function ThemeToggler() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return "dark";
+    }
+    return "light";
+  });
+
   const nextTheme = theme === "light" ? "dark" : "light";
 
   useEffect(() => {
-    document.body.dataset.theme = theme;
+    document.documentElement.dataset.theme = theme;
   }, [theme]);
 
   return (
