11bdacab7b0c3bca166ef1f3435c650dc7042a27
danielgen01 <gendik.waf@gmail.com>
2024-09-08 14:19:27 +0200

    feat:adjust Theme Switch Button

--- src/app/components/ToggleThemeButton/ToggleThemeButton.tsx
+++ src/app/components/ToggleThemeButton/ToggleThemeButton.tsx
@@ -1,12 +1,25 @@
 import { useTheme } from "next-themes";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { Tooltip } from "@mui/material";
 
 export function ThemeToggler() {
   const { theme, setTheme } = useTheme();
   const nextTheme = theme === "light" ? "dark" : "light";
 
+  const determineToolTip = () => {
+    if (theme === "light") {
+      return "Turn on the light ";
+    } else {
+      return "Turn off the light ";
+    }
+  };
+
   return (
-    <button onClick={() => setTheme(nextTheme)}>
-      Change to {nextTheme} mode
-    </button>
+    <Tooltip title={determineToolTip()}>
+      <button onClick={() => setTheme(nextTheme)}>
+        {theme === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+      </button>
+    </Tooltip>
   );
 }
