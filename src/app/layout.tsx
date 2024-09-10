910d0cbeefcc7d8ee9a44cc6e6ad806ebebd1238
danielgen01 <gendik.waf@gmail.com>
2024-09-10 15:37:32 +0200

    feat: get rid off tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./global.css"
 import Header from "./components/Header/Header";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { lightTheme } from "@/styles/theme";
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // Verwendet die 'class'-Strategie für den Dark Mode
-  plugins: [],
-}
