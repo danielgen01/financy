95c71127e1f6370dfd47d6ee38ec4db093b4eb36
danielgen01 <gendik.waf@gmail.com>
2024-09-14 22:31:19 +0200

    chore: add base64 loading for logo

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -80,9 +80,10 @@ const Header: React.FC = () => {
           alt="Logo_Financy"
           width={150}
           height={40}
-          // Add a placeholder or loader
           placeholder="blur"
-          blurDataURL="/placeholder.png"
+          blurDataURL={
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAHLSURBVHgB7VVBTsJAFP2/uJAAiUeoN2ABCUt6A0kgQXacQFwo7ii7GhfACXRHDRi8QWFnAgQO4KJHYCG4gX5nGhBJkc6UjYl9iybtvN/3Z96bGYAQIf47UIRUKlhJh3ACkiCi1vOLVoFjGigWLRVWaAGBCjJAsM1O9tyPpvgRaIm9IOIQIU2EerCBYn5QY0uUBFkQlU1Ts0WovzZQKgyu2J90kFevm12tL8remwHuOy5xQgBnIAFEeG13sjmpmn3iQUPHfX9XMyqQY4mUEFDLYwGusCErzmYx4+K2mgEk51GsBqfj+3hlpwEeOiK4AGkw31noVmzmzDbVj83E7Qiia5WyFbeyQUPX7mrNVHXREBF34Ti5NyNqfzfg+o4otHQ70gBTlng9XV3wHVMRKkKoDx/i083rifsMGDqMUC5985FkYWqKlPDQjY24/vObcpkfSIfOLQTK8dCBwk5KoX7RTkRjuvf7EUhX5/ys8D0p16HTNr7vjgkidTuvMbYOQYCONjIS/b1DIADXZ0X+Ol4r1EeGd+m3wz7I3H2qovvb83OEp6ERKx/inIAPmHgvkDjzPXYavfbjHbyO14eL9HW8CV1fxxmECBHir+MLosyshh2ygb4AAAAASUVORK5CYII="
+          }
         />
       </Link>
       <>
