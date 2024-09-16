bf45475cc9c8d5b2077e1ec233d7b16e9b8d2a95
danielgen01 <gendik.waf@gmail.com>
2024-09-16 14:00:02 +0200

    chore: listItem skeleton adjustments

--- src/app/components/BigCard/SubComponents.tsx
+++ src/app/components/BigCard/SubComponents.tsx
@@ -1,27 +1,20 @@
 import { Box } from "@mui/material";
 import MuiSkeleton from "../MuiSkeleton/MuiSkeleton";
+import styles from "./ListItem.styles.module.css";
+
+const CircularSkeletonActionIcon: React.FC = () => {
+  return <MuiSkeleton width={50} height={50} variant="circular" />;
+};
 
 export const BigCardSkeleton: React.FC = () => {
   return (
-    <Box display="flex" justifyContent="space-between">
-      <MuiSkeleton width="40%" height={75} />
-      <MuiSkeleton width="40%" height={75} />
-      <MuiSkeleton
-        sx={{
-          marginTop: "10px",
-        }}
-        width={50}
-        height={50}
-        variant="circular"
-      />
-      <MuiSkeleton
-        sx={{
-          marginTop: "10px",
-        }}
-        width={50}
-        height={50}
-        variant="circular"
-      />
+    <Box display="flex" justifyContent="space-between" alignItems={"center"}>
+      <MuiSkeleton width="30%" height={75} />
+      <MuiSkeleton width="30%" height={75} />
+      <div className={styles.StyledSkeletonActionIconsWrapper}>
+        <CircularSkeletonActionIcon />
+        <CircularSkeletonActionIcon />
+      </div>
     </Box>
   );
 };
