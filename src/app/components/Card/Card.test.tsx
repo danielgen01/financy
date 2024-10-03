b6f0fbcbf3c65eb1845032b818a605df4fb523e6
danielgen01 <gendik.waf@gmail.com>
2024-10-03 18:21:17 +0200

    feat: add basic testing card.tsx

--- src/app/components/Card/Card.test.tsx
+++ src/app/components/Card/Card.test.tsx
@@ -2,17 +2,41 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Card from "./Card";
 
-describe("Card Component", () => {
-  it("renders the card with title, amount, and performance", () => {
-    render(
-      <Card
-        title="Balance"
-        amount={1000}
-        performance={5}
-        isBalanceCard={false}
-      />,
-    );
+const defaultProps = {
+  title: "Test Title",
+  amount: 1000,
+  performance: 5.5,
+  isBalanceCard: false,
+};
 
-    expect(screen.getByText("Balance")).toBeInTheDocument();
+describe("Card Component", () => {
+  test("renders the card title", () => {
+    render(<Card {...defaultProps} />);
+    const titleElement = screen.getByText(/Test Title/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("renders the amount when provided", () => {
+    render(<Card {...defaultProps} />);
+    const amountElement = screen.getByText("€ 1000");
+    expect(amountElement).toBeInTheDocument();
+  });
+
+  test("renders performance with an up arrow", () => {
+    render(<Card {...defaultProps} />);
+    const performanceElement = screen.getByText("5.5%");
+    expect(performanceElement).toBeInTheDocument();
+
+    const arrowIcon = screen.getByRole("img", { hidden: true });
+    expect(arrowIcon).toHaveClass("svg-inline--fa");
+
+    screen.logTestingPlaygroundURL();
+  });
+
+  test("applies the correct styling based on isBalanceCard prop", () => {
+    const propsWithBalanceCard = { ...defaultProps, isBalanceCard: true };
+    render(<Card {...propsWithBalanceCard} />);
+    const amountElement = screen.getByText("€ 1000");
+    expect(amountElement).toHaveClass("StyledAmountPurpleColour");
   });
 });
