49af3f1ba6e1e0f640bc965f1d5bbdef6fa6af46
danielgen01 <gendik.waf@gmail.com>
2024-10-08 19:48:57 +0200

    feat: add basic cashflowOverview tests

---
+++ src/app/components/CashflowOverview/CashflowOverview.test.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import CashflowOverview, { CashflowOverviewProps } from "./CashflowOverview";
+import { useTheme } from "next-themes";
+
+jest.mock("next-themes", () => ({
+  useTheme: jest.fn(),
+}));
+
+// eslint-disable-next-line react/display-name
+jest.mock("../MuiSkeleton/MuiSkeleton", () => () => (
+  <div data-testid="mui-skeleton" />
+));
+
+jest.mock("./SubComponents", () => ({
+  roundToFixed: jest.fn((value) => value.toFixed(2)),
+}));
+
+describe("CashflowOverview Component", () => {
+  const mockUseTheme = useTheme as jest.Mock;
+
+  beforeEach(() => {
+    mockUseTheme.mockReturnValue({ theme: "light" });
+  });
+
+  const defaultProps: CashflowOverviewProps = {
+    netCashflowAmount: 1000,
+    incomeTotal: 2000,
+    expenseTotal: 1000,
+  };
+
+  it("displays income and expense totals correctly when values are provided", () => {
+    render(<CashflowOverview {...defaultProps} />);
+    expect(screen.getByText("2000.00")).toBeInTheDocument();
+    expect(screen.getByText("1000.00")).toBeInTheDocument();
+  });
+
+  it("renders MuiSkeleton components when incomeTotal is not provided", () => {
+    render(
+      <CashflowOverview
+        netCashflowAmount={0}
+        incomeTotal={0}
+        expenseTotal={0}
+      />,
+    );
+    expect(screen.getAllByTestId("mui-skeleton")).toHaveLength(3);
+  });
+
+  it("displays the net cashflow amount correctly when provided", () => {
+    render(<CashflowOverview {...defaultProps} />);
+    expect(screen.getByText("€ 1000.00")).toBeInTheDocument();
+  });
+});
