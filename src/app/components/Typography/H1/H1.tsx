67164f48904358258515153201d1a97652041f93
danielgen01 <gendik.waf@gmail.com>
2024-10-26 17:13:54 +0200

    feat: adjust H1 styling

--- src/app/components/Typography/H1/H1.tsx
+++ src/app/components/Typography/H1/H1.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./H1.styles.module.css";
 
-const H1: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const H1: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return <h1 className={styles.StyledH1}>{children}</h1>;
 };
-
-export default H1;
