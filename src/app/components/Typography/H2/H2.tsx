bde03f2db887973a66ac3c5e59ea3a6e67c43911
danielgen01 <gendik.waf@gmail.com>
2024-10-26 17:14:05 +0200

    feat: add H2 component

---
+++ src/app/components/Typography/H2/H2.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./H2.styles.module.css";
+
+export const H2: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <h2 className={styles.StyledH2}>{children}</h2>;
+};
