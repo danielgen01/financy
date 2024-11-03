614d3d2c21b8e5a0f0aea9fc083664ecb0ca7b09
danielgen01 <gendik.waf@gmail.com>
2024-11-03 09:50:19 +0100

    feat: use global image styling

--- src/app/components/BigBenefitTeaser/BigBenefitTeaser.tsx
+++ src/app/components/BigBenefitTeaser/BigBenefitTeaser.tsx
@@ -22,10 +22,7 @@ export const BigBenefitTeaser: React.FC<BigBenefitTeaserProps> = ({
       <Image
         {...image}
         alt={image.alt || "default_alt_text"}
-        style={{
-          maxWidth: "100%",
-          height: "auto",
-        }}
+        className="ResponsiveImage"
       />
     </div>
   )
--- src/app/pageTemplates/homepage/SubComponents.tsx
+++ src/app/pageTemplates/homepage/SubComponents.tsx
@@ -78,7 +78,7 @@ export const PreviewSection: React.FC = () => {
         <Image
           src={imageSrc}
           alt="Preview_of_financy"
-          style={{ maxWidth: "100%", height: "auto" }}
+          className="ResponsiveImage"
           placeholder="blur"
         />
       </div>
