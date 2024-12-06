ee9d0d3e076985b2104d73fbb202cb3156cbf7ca
danielgen01 <gendik.waf@gmail.com>
2024-12-06 15:51:03 +0100

    feat: some design fixes for the footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,20 +1,40 @@
-import React from "react"
-import styles from "./Footer.styles.module.css"
-import LogoDark from "../../../../public/Logo-dark.png"
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "next/image"
+import React from "react"
+
+import LogoDark from "../../../../public/Logo-dark.png"
+import styles from "./Footer.styles.module.css"
 
 export const Footer = () => {
   return (
     <div className={styles.StyledFooter}>
       <div className={styles.StyledFooterContent}>
-        <Image src={LogoDark} alt="Logo" />
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ipsum
-          at dolorem delectus amet debitis laborum aperiam, cumque, repudiandae
-          dignissimos aspernatur ea exercitationem vero a est. Sapiente qui
-          expedita optio!
-        </span>
+        <div className={styles.StyledFooterHead}>
+          <Image src={LogoDark} alt="Logo" />
+          <span className={styles.StyledFooterHeadline}>
+            Subscribe to Newsletter
+          </span>
+          <EmailNewsletterInput />
+        </div>
+        <hr className={styles.StyledLine} />
       </div>
+    </div>
+  )
+}
+
+const EmailNewsletterInput = () => {
+  return (
+    <div className={styles.StyledInputBox}>
+      <input
+        className={styles.StyledInputField}
+        placeholder="Enter your E-Mail Adress"
+        minLength={5}
+        type="email"
+      />
+      <button type="button" className={styles.StyledInputButton}>
+        <FontAwesomeIcon icon={faPaperPlane} size="2x" />
+      </button>
     </div>
   )
 }
